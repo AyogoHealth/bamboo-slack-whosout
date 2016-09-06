35cec4a89b6be4531c8b5a5e67e9626cae501fbf
Neil Saunders <neil@beamly.com>
2016-09-06 17:43:23 +0100

    Add return dates to Slack message

--- bamboo.py
+++ bamboo.py
@@ -42,14 +42,14 @@ bamboodomains = {
 }
 
 def whosout(today):
-  out = []  
+  out = []
   bamboorequest = Request("https://{0}/api/gateway.php/{1}/v1/time_off/whos_out/?filter=off&end={2}".format(bamboodomains[BAMBOO_DOMAIN], BAMBOO_ACCOUNT, today.strftime("%Y-%m-%d")))
 
   bamboorequest.add_header("Authorization", "Basic %s" % base64string)
   bamboorequest.add_header("Accept", "application/json")
   try:
     result = urlopen(bamboorequest)
-    return json.loads(result.read())   
+    return json.loads(result.read())
   except HTTPError as e:
     logger.error("Request failed: %d %s", e.code, e.reason)
   except URLError as e:
@@ -81,6 +81,23 @@ def posttoslack(text):
   except URLError as e:
     logger.error("Server connection failed: %s", e.reason)
 
+def get_return_time_friendly(return_date):
+  today_datetime_exact = datetime.today()
+  today_datetime_date_only = datetime.strptime(today_datetime_exact.strftime("%Y-%m-%d"), "%Y-%m-%d")
+  return_datetime = datetime.strptime(return_date, "%Y-%m-%d")
+  days_difference_delta = return_datetime - today_datetime_date_only
+  days_difference = days_difference_delta.days
+  todays_week_number = today_datetime_exact.isocalendar()[1]
+  return_week_number = return_datetime.isocalendar()[1]
+
+  if days_difference == 1:
+    return "Tomorrow"
+  elif todays_week_number == return_week_number:
+    return return_datetime.strftime("%A")
+  elif days_difference <= 7:
+    return return_datetime.strftime("Next %A, %B %d")
+  else:
+    return return_datetime.strftime("%A, %B %d")
 
 def lambda_handler(event, context):
   today = datetime.now()
@@ -93,7 +110,7 @@ def lambda_handler(event, context):
       if who.get("type") == "holiday":
         posttoslack("*Today is:* {} :confetti_ball:".format(who.get("name")))
       else:
-        names.append(who.get("name"))
+        names.append("{0} (Back {1})".format(who.get("name"), who.get("end")))
 
     if names:
       text = "*Who's out today:* {}".format(', '.join(names))
